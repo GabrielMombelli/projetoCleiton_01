87fd9f9affa32afc1bdb165f8568812c6fda4060
Gabriel Henrique Mombelli <168040285+GabrielMombelli@users.noreply.github.com>
2024-08-19 22:29:19 -0300

    Add files via upload

---
+++ Atividade01_ManipulacaoVariavel/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //  Criação de Variáveis
+        int idade = 55;
+        double altura = 2.5;
+        String nome = "Ygor";
+        boolean estudante = true;
+
+        //  Operações Simples
+        int somaIdades = idade + 30;
+        double alturaDobro = altura * 2;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nome: " + nome);
+        System.out.println("Estudante: " + estudante);
+        System.out.println("Soma Idades: " + somaIdades);
+        System.out.println("Dobro da altura: " + alturaDobro);
+        System.out.println("Olá " + nome);
+
+    }
+}
